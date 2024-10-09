933a413f4a3cf1fa821bdafa8119da05033445fc
Hilario Nengare <hnengare@gmail.com>
2024-10-09 11:11:59 +0300

    feat: Add initial implementation of WorkFlowAutomation class

--- WorkFlowAutomation.cs
+++ WorkFlowAutomation.cs
@@ -2,3 +2,17 @@
 *   Author: Hilario Junior Nengare
 *   Deliverable: Automated Doc Approval Workflow Console Application
 */
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+public class WorkFlowAutomation{
+
+    public static void main(str[] args){
+
+        console.WriteLine("Hello World");
+    }
+}
