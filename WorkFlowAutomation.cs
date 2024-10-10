90b735eac75dc2124efe7bb73260193203350109
Hilario Nengare <hnengare@gmail.com>
2024-10-10 06:43:04 +0300

    feat: Add Document class and WriteFileEngine for workflow automation

--- WorkFlowAutomation.cs
+++ WorkFlowAutomation.cs
@@ -4,6 +4,7 @@
 */
 
 using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,45 @@ namespace WorkflowAutomation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+
+    class Document{
+
+        public string Name {get; set;}
+        public string Path {get; set;}
+        public string Description {get; set;}
+        public string Submitter {get; set;}
+        public Status DocumentStatus {get; set;}
+        
+        public Document(string name, string path, string description, string submitter){
+            Name = name;
+            Path = path;
+            Description = description;
+            Submitter = submitter;
+            DocumentStatus = Status.Submitted;
+        }
+    }
+
+    enum Status {
+        Submitted,
+        Approved,
+        Rejected
+    }
+
+    class WriteFileEngine{
+        public static void processDocument(Document doc){
+            string content =  File.ReadAllText(doc.Path);
+
+            if(String.IsNullOrEmpty(content)){
+                doc.DocumentStatus = Status.Rejected;
+                Console.WriteLine($"The above file has been accepted.");
+            }
+            else{
+                doc.DocumentStatus = Status.Submitted;
+                Console.WriteLine("The above file has been rejected.");
+            }
         }
     }
 }
